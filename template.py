cf634cd14614a3669e8be7bbc000879c3f9b343d
Fayomi Kehinde <fayomik02@gmail.com>
2025-07-22 13:27:10 +0200

    update1

---
+++ template.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+project_nmae = "mlProject"
+
+list_of_files = [
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    f"src/{project_name}/___init__,py",
+    
+]
